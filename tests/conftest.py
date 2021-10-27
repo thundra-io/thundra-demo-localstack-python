d166a944e60e130a52c70d1b9153b0c3e0c391df
gokhan721 <simsek.gokhan721@gmail.com>
2021-10-27 13:01:04 +0300

    fix api_key and project_id

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,8 +10,8 @@ import thundra
 thundra.configure(
     options={
         "config": {
-            "thundra.apikey": "6327942a-36ff-40a5-a840-98e71cc2af7e",
-            "thundra.agent.test.project.id": "91596bb4-f5cf-4c28-9008-7eead02bd70f",
+            "thundra.apikey": "api_key",
+            "thundra.agent.test.project.id": "test_project_id",
             "thundra.agent.application.name": "foresight_deneme_genel",
             "thundra.agent.debug.enable": "True",
             "thundra.agent.report.rest.baseurl": "https://collector.thundra.me/v1",
